31de3d6f403934b270ecb1b454bdb424b985a002
Petko Pedev <petko.pedev@gmail.com>
2019-04-28 23:38:03 +0200

    Adding Forum to the repository. Basic CRUD capabilities of the project are implemented. Additional functionality is left to be added.

---
+++ Forum/Forum/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Forum.Models;
+using Forum.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ForumDbContext context;
+
+        public HomeController(ForumDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Topic> topics = context.Topics
+                .Include(t => t.Author)
+                .OrderByDescending(t => t.CreatedDate)
+                .ThenByDescending(t => t.LastUpdatedDate)
+                .ToList();
+
+            return View(topics);
+        }
+    }
+}
